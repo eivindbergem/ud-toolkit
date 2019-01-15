7e015310cf598121d9b0eb342c17ab3c8d22e312
Eivind Alexander Bergem <eivind.bergem@gmail.com>
2019-01-15 10:36:33 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ud-toolkit',
-      version='0.0.1',
+      version='0.0.2',
       description='NLP toolkit built around UDPipe.',
       url='http://github.com/eivindbergem/ud-toolkit',
       author='Eivind Alexander Bergem',
